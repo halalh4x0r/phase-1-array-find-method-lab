35b1a94aef1695740322c0c686c6bb6a9c8c56d5
halalh4x0r <mohamed.hadi@student.moringaschool.com>
2025-07-11 11:50:16 +0300

    modified a file

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function superbowlWin(record) {
+  const win = record.find(entry => entry.result === "W");
+  return win ? win.year : undefined;
+}
+
+const record = [
+  { year: "2015", result: "N/A" },
+  { year: "2016", result: "N/A" },
+  { year: "2017", result: "W" },
+  { year: "2018", result: "L" },
+];
+
+console.log(superbowlWin(record)); 
